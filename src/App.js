915aacbda4e20120e357abecfbdeceeb52b8a8fb
Alona S <alona.s.kry@gmail.com>
2025-02-07 13:20:52 +0000

    define main components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useState} from "react";
+import {v4} from "uuid";
+
+function App() {
+
+
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default App;
