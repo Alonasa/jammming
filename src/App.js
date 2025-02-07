7501b9961a7f93e1a386a259a8b7c1748e3af4f8
Alona S <alona.s.kry@gmail.com>
2025-02-07 19:46:00 +0000

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {useState} from "react";
 import {v4} from "uuid";
+import {SearchBar} from "./components/SearchBar/SearchBar.tsx";
 
 function App() {
-
-
+    let options = ["hello", "bye"]
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <h1>Jammming</h1>
+            <SearchBar options={options}/>
         </div>
     );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import styles from './SearchBar.module.css';
+import {Button} from "@mui/material"; // Import the CSS module
 
-export const SearchBar = () => {
+
+
+type OptType = {
+    options: string[]
+}
+
+export const SearchBar = ({options}: OptType) => {
     return(
-        <div>
-
+        <div className={styles.main}>
+            <TextField color="inherit" className={styles.inputField} id="outlined-basic" label="Search song" variant="outlined"/>
+            <Button className={styles.button} color="inherit" type="button" variant="outlined">Search</Button>
         </div>
+
     )
 
 }
